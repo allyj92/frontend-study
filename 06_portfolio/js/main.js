c52112beab30e240196b0d27d24b4c72607ecc84
eunjaelim <imeunjae361@gmail.com>
2024-08-01 00:12:34 +0900

    portpolio at home to 2024-07-31

--- 06_portfolio/js/main.js
+++ 06_portfolio/js/main.js
@@ -1,5 +1,5 @@
 // ScrollMagic 사용법
-let spyEls = document.querySelectorAll("section.scroll-spy");
+let spyEls = document.querySelectorAll(".scroll-spy");
 console.log(spyEls);
 
 spyEls.forEach(function (spyEl) {
@@ -14,30 +14,24 @@ spyEls.forEach(function (spyEl) {
 
 // 모달창 띄우기
 document.addEventListener("DOMContentLoaded", function () {
-  // Get modal elements
   const modal = document.getElementById("modal");
   const btnClose = document.querySelector(".btn-close");
   const btnModals = document.querySelectorAll(".btn-modal");
 
-  // Function to open the modal
   function openModal() {
     modal.style.display = "block";
   }
 
-  // Function to close the modal
   function closeModal() {
     modal.style.display = "none";
   }
 
-  // Attach click event to open buttons
   btnModals.forEach((btn) => {
     btn.addEventListener("click", openModal);
   });
 
-  // Attach click event to close button
   btnClose.addEventListener("click", closeModal);
 
-  // Close modal if user clicks outside the modal content
   window.addEventListener("click", function (event) {
     if (event.target === modal) {
       closeModal();
@@ -45,10 +39,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// course
 document.addEventListener("DOMContentLoaded", function () {
-  const courses = document.querySelectorAll(".inner-list .inner"); // Select all course elements
-  const totalCourses = courses.length; // Total number of courses
+  const courses = document.querySelectorAll(".inner-list .inner");
+  const totalCourses = courses.length;
   let currentCourseIndex = 0;
 
   function showCurrentCourse() {
@@ -73,13 +66,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const rightArrow = document.querySelector(".right-arrow");
 
   leftArrow.addEventListener("click", function () {
-    clearInterval(intervalId); // Stop automatic transition
+    clearInterval(intervalId);
     currentCourseIndex = (currentCourseIndex - 1 + totalCourses) % totalCourses;
     showCurrentCourse();
   });
 
   rightArrow.addEventListener("click", function () {
-    clearInterval(intervalId); // Stop automatic transition
+    clearInterval(intervalId);
     currentCourseIndex = (currentCourseIndex + 1) % totalCourses;
     showCurrentCourse();
   });
